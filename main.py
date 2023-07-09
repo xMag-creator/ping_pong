62ca477cf89c00c7d3d25aa04264a07708b4a1e2
jerry <p.kobylarz@outlook.com>
2023-07-09 20:58:09 +0200

    ball is moving

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pgzrun
-from random import randint
+from random import randint, choice
+
+
+# define additional functions
+def update_ball_position():
+    # x axis update
+    if ball.direction_x == "left":
+        ball.x -= ball.speed
+    elif ball.direction_x == "right":
+        ball.x += ball.speed
+
+    # y axis update
+    if ball.direction_y == "up":
+        ball.y -= ball.speed
+    elif ball.direction_y == "down":
+        ball.y += ball.speed
+
+    # checking if ball hit something
+    if ball.x < 5:
+        ball.direction_x = "right"
+    elif ball.x > WIDTH - 5:
+        ball.direction_x = "left"
+
+    # checking if someone win
+    if ball.y < 5:
+        ball.winner = "PLAYER B"
+    elif ball.y > HEIGHT - 5:
+        ball.winner = "PLAYER A"
 
 
 # start program
@@ -19,16 +46,20 @@ ball = Actor("ball.png")
 ball.y = HEIGHT // 2
 ball.x = WIDTH // 2
 
+# additional variables
+ball.direction_x = choice(("left", "right"))
+ball.direction_y = choice(("up", "down"))
+ball.speed = 2
+ball.winner = None
+
 
 def update():
     # control functions
-
-    pass
+    update_ball_position()
 
 
 def draw():
     # generating screen
-
     screen.blit("nature-2384_1280.jpg", (0, 0))
     palette_a.draw()
     palette_b.draw()
